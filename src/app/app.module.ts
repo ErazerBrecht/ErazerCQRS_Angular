813bc39d2188c6ebd3bc61b4b1476bbd03f789a6
Brecht Carlier (Involved) <brecht.carlier@involved-it.be>
2017-07-16 16:23:49 +0200

    Added navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponentComponent } from './navbar-component/navbar-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponentComponent
   ],
   imports: [
     BrowserModule
