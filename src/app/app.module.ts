91c97fc19326c8798368f86626315372cbbb8d95
Brecht Carlier (Involved) <brecht.carlier@involved-it.be>
2017-07-18 22:48:38 +0200

    Added fonts Added basic components Edited folder structure to seperate container components/view components Renamed navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponentComponent } from './navbar-component/navbar-component.component';
+
+// Containers (Smart - components)
+import { NavbarComponent } from './containers/navbar/navbar.component';
+import { AllTicketsComponent } from './containers/all-tickets/all-tickets.component';
+
+// Presential Components (Dumb - components)
+import { ListComponent } from './components/list/list.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponentComponent
+    NavbarComponent,
+    AllTicketsComponent,
+    ListComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule
--- src/app/components/welcome/welcome.component.spec.ts
+++ src/app/components/welcome/welcome.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavbarComponentComponent } from './navbar-component.component';
+import { WelcomeComponent } from './welcome.component';
 
-describe('NavbarComponentComponent', () => {
-  let component: NavbarComponentComponent;
-  let fixture: ComponentFixture<NavbarComponentComponent>;
+describe('WelcomeComponent', () => {
+  let component: WelcomeComponent;
+  let fixture: ComponentFixture<WelcomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavbarComponentComponent ]
+      declarations: [ WelcomeComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavbarComponentComponent);
+    fixture = TestBed.createComponent(WelcomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
